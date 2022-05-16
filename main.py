599aee7ac74929d0206bf20053e0eb2c81001105
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-05-16 15:18:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from pytgcalls import idle
+from Angel.main import call_py, bot, BOT
+
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[INFO]: STARTING PYTGCALLSS CLIENT")
+    await call_py.start()
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
+BOT.run_until_disconnected()
