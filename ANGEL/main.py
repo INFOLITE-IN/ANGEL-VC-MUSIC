929d985d0809583d3926a590051ae0437a82b821
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-05-17 07:38:06 +0530

    Create main.py

---
+++ ANGEL/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import random
+import asyncio
+import telethon.utils
+from telethon import TelegramClient, events
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+
+bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "NIXA.Player"},
+)
+
+BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+user = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+
+Test = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'NIXA.Player'})
+call_py = PyTgCalls(Test)
