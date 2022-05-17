02d095520d96fcd9394841a838b34b3f64783df2
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-05-17 07:47:57 +0530

    Create id.py

---
+++ ANGEL/Player/id.py
@@ -0,0 +1,15 @@
+from ANGEL.main import bot
+from pyrogram import filters
+
+
+@bot.on_message(filters.command('id'))
+def ids(_, message):
+    reply = message.reply_to_message
+    if reply:
+        message.reply_text(
+            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+        )
+    else:
+        message.reply(
+            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
+        )
